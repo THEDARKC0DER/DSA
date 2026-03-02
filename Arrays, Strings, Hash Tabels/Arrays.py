92f7cb12881f3d49c4d6788fb1523874be451e87
Ayush Agrawal <61264256+THEDARKC0DER@users.noreply.github.com>
2026-03-02 14:01:28 +0000

    Arrays

---
+++ Arrays, Strings, Hash Tabels/Arrays.py
@@ -0,0 +1,11 @@
+# Arrays: The Building Blocks of Data Structures
+# provide a simple yet effective way to store a fixed-size, sequential collection of elements of the same data type.
+# Properties of Array:
+# Homogenous 
+# Contiguous memory
+# Fixed size
+
+Array Patterns and Techniques
+1. Two Pointers -  Finding pairs, in-place sorting (like quicksort partition), or reversing an array.
+2. Sliding Window - Finding the longest/shortest subarray with a specific characteristic (e.g., maximum sum sub-array of size k).
+3. Prefix Sum -  Range sum queries, dynamic programming problems.
