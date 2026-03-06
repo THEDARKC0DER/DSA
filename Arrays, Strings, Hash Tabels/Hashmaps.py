4bbe00002bf1dcab494dda90397584d5ed819662
Ayush Agrawal <61264256+THEDARKC0DER@users.noreply.github.com>
2026-03-06 18:12:31 +0000

    links

---
+++ Arrays, Strings, Hash Tabels/Hashmaps.py
@@ -0,0 +1,30 @@
+A Hash Map, is a data structure used to implement an associative array abstract data type, 
+a structure that can map keys to values.
+
+Components of a HashMap:
+1)The Array (or Bucket Array)
+2)The Key-Value Pairs
+3)The Hash Function
+
+The Hash Function
+The hash function's primary role is to distribute keys uniformly across the array. 
+A good hash function is fast to compute and minimizes collisions.
+
+Key Aspects of a Hash Function
+Determinism
+Uniformity
+Speed
+
+
+Collision Resolution Strategies
+1. Separate Chaining
+2. Open Addressing (Probing)
+Linear Probing
+Quadratic Probing
+Double Hashing
+
+
+Core Use Cases and Benefits
+1. Frequency Counting and Tallying
+2. Efficient Lookups and Existence Checks
+3. Mapping and Relationships
